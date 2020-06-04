61df63b85657e2d9cec25a2686b7541ce1d9e3d0
adipiriu <adi.piriu@gmail.com>
2020-06-04 22:33:51 +0300

    Added static react files

---
+++ client/build/precache-manifest.1924f3293ac4c7669dd5f6806fe90ad6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555ed25f5a51f5784dd8499b46d46991",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97eb9be51b6ba31abb63",
+    "url": "/static/css/main.3e05e983.chunk.css"
+  },
+  {
+    "revision": "a1d39573c1f748adc2fc",
+    "url": "/static/js/2.f8016159.chunk.js"
+  },
+  {
+    "revision": "cfc6abbc9aa3d8ace89d2d2a27d133c8",
+    "url": "/static/js/2.f8016159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97eb9be51b6ba31abb63",
+    "url": "/static/js/main.65ef4952.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
